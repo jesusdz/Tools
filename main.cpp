230ce5c9d261bf22caedd3ab3e1a5a179b252484
Jesús Díaz García <jesusdz@gmail.com>
2022-09-27 00:26:22 +0200

    Advances in scanning.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <string>
+#include <string.h>
+#include <assert.h>
 
 #define KB(x) (1024 * x)
 #define MB(x) (1024 * KB(x))
 #define TB(x) (1024 * MB(x))
 
+#define ASSERT(expression) assert(expression)
+
 #define COMMAND_NAME "jsl"
 #define MAX_LINE_SIZE KB(1)
 #define MAX_TOKEN_COUNT KB(10)
+#define MAX_LEXEME_SIZE 128
 
 
 
@@ -22,19 +28,56 @@ const char *TokenNames[] = {
 };
 #undef ENUM_ENTRY
 
+enum LiteralType
+{
+	LITERAL_BOOL,
+	LITERAL_INT,
+	LITERAL_FLOAT,
+};
+
+struct Literal
+{
+	LiteralType type;
+	union
+	{
+		bool b;
+		int i;
+		float f;
+	};
+};
+
 struct Token
 {
     TokenType type;
+	std::string lexeme;
+	Literal literal;
+	int line;
 };
 
-unsigned int readEntireFile(const char *filename, char *bytes, unsigned int bytesSize)
+struct TokenList
+{
+	Token tokens[MAX_TOKEN_COUNT];
+	int count;
+};
+
+struct ScanState
+{
+	int start;
+	int current;
+	int line;
+
+	const char *script;
+	char scriptSize;
+};
+
+unsigned int ReadEntireFile(const char *filename, char *bytes, unsigned int bytesSize)
 {
 	unsigned int bytesRead = 0;
 	FILE *f = fopen(filename, "rb");
 	if ( f )
 	{
 		bytesRead = fread(bytes, sizeof(unsigned char), bytesSize, f);
-		// TODO: Assert bytesRead <= byesSize
+		ASSERT(bytesRead <= bytesSize);
 		bytes[bytesRead] = 0;
 		fclose(f);
 	}
@@ -46,34 +89,97 @@ const char* TokenName(TokenType type)
 	return TokenNames[type];
 }
 
-unsigned int ScanTokens(Token tokens[MAX_TOKEN_COUNT], const char *script, unsigned int scriptSize)
+bool IsAtEnd(const ScanState &scanState)
 {
-	// TODO
-	return 0;
+	return scanState.current >= scanState.scriptSize;
 }
 
-void run(const char *script, unsigned int scriptSize)
+char Advance(ScanState &scanState)
 {
-	Token tokens[MAX_TOKEN_COUNT] = {};
-	unsigned int tokenCount = ScanTokens(tokens, script, scriptSize);
+	ASSERT(scanState.current < scanState.scriptSize);
+	return scanState.script[scanState.current++];
+}
 
-	for (unsigned int tokenIdx = 0; tokenIdx < tokenCount; ++tokenIdx)
+void AddToken(ScanState &scanState, TokenList &tokenList, TokenType tokenType, Literal literal)
+{
+	Token newToken = {};
+	newToken.type = tokenType;
+
+	char lexeme[MAX_LEXEME_SIZE];
+	strncpy(lexeme, scanState.script + scanState.start, scanState.current - scanState.start);
+	newToken.lexeme = lexeme;
+	newToken.literal = literal;
+	newToken.line = scanState.line;
+
+	tokenList.tokens[tokenList.count++] = newToken;
+}
+
+void AddToken(ScanState &scanState, TokenList &tokenList, TokenType tokenType)
+{
+	Literal nullLiteral = {};
+	AddToken(scanState, tokenList, tokenType, nullLiteral);
+}
+
+void ScanToken(ScanState &scanState, TokenList &tokenList)
+{
+	char c = Advance(scanState);
+
+	switch (c)
 	{
-		const Token &token = tokens[tokenIdx];
+		case '(': AddToken(scanState, tokenList, TOKEN_LEFT_PAREN); break;
+		case ')': AddToken(scanState, tokenList, TOKEN_RIGHT_PAREN); break;
+		case '{': AddToken(scanState, tokenList, TOKEN_LEFT_BRACE); break;
+		case '}': AddToken(scanState, tokenList, TOKEN_RIGHT_BRACE); break;
+		case ',': AddToken(scanState, tokenList, TOKEN_COMMA); break;
+		case '.': AddToken(scanState, tokenList, TOKEN_DOT); break;
+		case '-': AddToken(scanState, tokenList, TOKEN_MINUS); break;
+		case '+': AddToken(scanState, tokenList, TOKEN_PLUS); break;
+		case '*': AddToken(scanState, tokenList, TOKEN_STAR); break;
+		case ';': AddToken(scanState, tokenList, TOKEN_SEMICOLON); break;
+	}
+}
+
+TokenList ScanTokens(const char *script, unsigned int scriptSize)
+{
+	TokenList tokenList = {};
+
+	ScanState scanState = {};
+	scanState.line = 1;
+	scanState.script = script;
+	scanState.scriptSize = scriptSize;
+
+	while ( !IsAtEnd(scanState) )
+	{
+		scanState.start = scanState.current;
+		ScanToken(scanState, tokenList);
+	}
+
+	AddToken(scanState, tokenList, TOKEN_EOF);
+
+	return tokenList;
+}
+
+void Run(const char *script, unsigned int scriptSize)
+{
+	TokenList tokenList = ScanTokens(script, scriptSize);
+
+	for (unsigned int tokenIdx = 0; tokenIdx < tokenList.count; ++tokenIdx)
+	{
+		const Token &token = tokenList.tokens[tokenIdx];
 		printf("%s ", TokenName(token.type));
 	}
 	printf("\n");
 }
 
-void runFile(const char* filename)
+void RunFile(const char* filename)
 {
 	char *bytes = new char[MB(1)]();
-	unsigned int bytesRead = readEntireFile(filename, bytes, MB(1));
-	run(bytes, bytesRead);
+	unsigned int bytesRead = ReadEntireFile(filename, bytes, MB(1));
+	Run(bytes, bytesRead);
 	delete[] bytes;
 }
 
-void runPrompt()
+void RunPrompt()
 {
 	char line[MAX_LINE_SIZE];
 	unsigned int lineSize = 0;
@@ -82,7 +188,7 @@ void runPrompt()
 		printf("> ");
 		// TODO: Read line
 		// TODO: Break if exit command
-		run(line, lineSize);
+		Run(line, lineSize);
 	}
 }
 
@@ -95,11 +201,11 @@ int main(int argc, char **argv)
 	}
 	else if ( argc == 2 )
 	{
-		runFile(argv[1]);
+		RunFile(argv[1]);
 	}
 	else
 	{
-		runPrompt();
+		RunPrompt();
 	}
     return 0;
 }
