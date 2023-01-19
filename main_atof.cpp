805de7e45d6a0e005c437bf328cd206ba5112f7a
Jesús <jesusdz@gmail.com>
2023-01-20 00:39:44 +0100

    main_atof.cpp: Added functional and performance tests. Custom version of atof is working faaaast!

--- main_atof.cpp
+++ main_atof.cpp
@@ -1,5 +1,8 @@
 #include "tools.h"
 
+#define TEST_FUNCTIONALITY 1
+#define TEST_PERFORMANCE 1
+
 f32 myatof(const char *str)
 {
 	u32 integer = 0;
@@ -44,12 +47,48 @@ int main()
 {
 	const char *numbers [] = { "0", "0.0f", "10.54", "10.54f", ".43", ".43f", "5.f" };
 
+#if TEST_FUNCTIONALITY
+	LOG(Info, "Functional test:\n");
+
 	for ( u32 i = 0; i < ARRAY_COUNT(numbers); ++i )
 	{
 		const char *numberStr = numbers[i];
-		LOG(Info, "atof: %f\n", atof(numberStr));
-		LOG(Info, "myatof: %f\n", myatof(numberStr));
+		LOG(Info, "- atof: %f\n", atof(numberStr));
+		LOG(Info, "- myatof: %f\n", myatof(numberStr));
 	}
+#endif
+
+#if TEST_PERFORMANCE
+	LOG(Info, "Performance test:\n");
+
+	float sum = 0.0f;
+
+	Clock c0 = GetClock();
+	for ( u32 j = 0; j < 999999; ++j )
+	{
+		for ( u32 i = 0; i < ARRAY_COUNT(numbers); ++i )
+		{
+			const char *numberStr = numbers[i];
+			sum += atof(numberStr);
+		}
+	}
+
+	Clock c1 = GetClock();
+	for ( u32 j = 0; j < 999999; ++j )
+	{
+		for ( u32 i = 0; i < ARRAY_COUNT(numbers); ++i )
+		{
+			const char *numberStr = numbers[i];
+			sum += myatof(numberStr);
+		}
+	}
+
+	Clock c2 = GetClock();
+
+	LOG(Info, "- atof: %f seconds\n", GetSecondsElapsed(c0, c1));
+	LOG(Info, "- myatof: %f seconds\n", GetSecondsElapsed(c1, c2));
+#endif
+
 	return 0;
 }
 
