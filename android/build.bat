38cd7a6cc303224fceb9e6368ec616a9efd0800c
Jesús <jesusdz@gmail.com>
2023-02-12 20:05:06 +0100

    build.bat update

--- android/build.bat
+++ android/build.bat
@@ -11,18 +11,28 @@ set PLATFORM_TOOLS=%ANDROID_HOME%\platform-tools
 set ANDROID_PLATFORM_DIR=%ANDROID_HOME%\platforms\android-33
 set APPLICATION_REL_PATH=com\tools\game
 set HOST=windows-x86_64
+
+REM clang.exe --print-targets to see a full list of targets
+REM set TARGET=arm64-linux-android33
 set TARGET=aarch64-linux-android33
 
 REM NDK
 set NDK=%ANDROID_HOME%\ndk\25.2.9519653
 set TOOLCHAIN=%NDK%\toolchains\llvm\prebuilt\%HOST%
-set GCC=%TOOLCHAIN%\bin\%TARGET%-clang
-set GXX=%TOOLCHAIN%\bin\%TARGET%-clang++
+REM set GCC=%TOOLCHAIN%\bin\%TARGET%-clang
+REM set GXX=%TOOLCHAIN%\bin\%TARGET%-clang++
+set GCC=%TOOLCHAIN%\bin\clang --target=%TARGET% -march=armv8-a
+set GXX=%TOOLCHAIN%\bin\clang++ --target=%TARGET% -march=armv8-a
 set AR=%TOOLCHAIN%\bin\llvm-ar
 set TOOLCHAIN_LIB_DIR=%TOOLCHAIN%\sysroot\usr\lib\aarch64-linux-android
 
 REM Include dir for native_app_glue sources .h/.cpp
 set NATIVE_APP_GLUE_DIR=%NDK%\sources\android\native_app_glue
+
+set OUT_LIB_DIR=lib\arm64-v8a
+REM set OUT_LIB_DIR=lib\armeabi-v7a
+REM set OUT_LIB_DIR=lib\x86
+REM set OUT_LIB_DIR=lib\x86_64
 
 
 
@@ -50,24 +60,24 @@ REM ######################################################
 
 rmdir /S /Q obj lib 2> nul
 mkdir obj lib 2> nul
-mkdir lib\arm64-v8a 2> nul
+mkdir %OUT_LIB_DIR% 2> nul
 
 pushd obj
 
 REM Have a look at this compile arguments
 REM /Users/thomas/Documents/android-ndk-r5b/toolchains/arm-eabi-4.4.0/prebuilt/darwin-x86/bin/arm-eabi-g++ --sysroot=/Users/thomas/Documents/android-ndk-r5b/platforms/android-8/arch-arm -march=armv7-a -mfloat-abi=softfp -mfpu=neon -Wl,--fix-cortex-a8 -fno-exceptions -fno-rtti -nostdlib -fpic -shared -o GLmove.so -O3
 
-echo %GCC% -march=armv8-a  -fPIC -c %NATIVE_APP_GLUE_DIR%\android_native_app_glue.c -o android_native_app_glue.o
-call %GCC% -march=armv8-a  -fPIC -c %NATIVE_APP_GLUE_DIR%\android_native_app_glue.c -o android_native_app_glue.o
+echo %GCC% -fPIC -c %NATIVE_APP_GLUE_DIR%\android_native_app_glue.c -o android_native_app_glue.o
+call %GCC% -fPIC -c %NATIVE_APP_GLUE_DIR%\android_native_app_glue.c -o android_native_app_glue.o
 
 echo %AR% rcs libandroid_native_app_glue.a android_native_app_glue.o
 call %AR% rcs libandroid_native_app_glue.a android_native_app_glue.o
 
-echo %GXX% -I%NATIVE_APP_GLUE_DIR% -march=armv8-a --sysroot=%TOOLCHAIN%\sysroot -std=gnu++11 -fPIC -c ..\jni\main.cpp -o main.o
-call %GXX% -I%NATIVE_APP_GLUE_DIR% -march=armv8-a --sysroot=%TOOLCHAIN%\sysroot -std=gnu++11 -fPIC -c ..\jni\main.cpp -o main.o
+echo %GXX% -I%NATIVE_APP_GLUE_DIR% --sysroot=%TOOLCHAIN%\sysroot -std=gnu++11 -fPIC -c ..\jni\main.cpp -o main.o
+call %GXX% -I%NATIVE_APP_GLUE_DIR% --sysroot=%TOOLCHAIN%\sysroot -std=gnu++11 -fPIC -c ..\jni\main.cpp -o main.o
 
-echo %GXX% main.o -L. -landroid_native_app_glue -landroid -lEGL -lGLESv1_CM -llog -shared -o ..\lib\arm64-v8a\libgame.so
-call %GXX% main.o -L. -landroid_native_app_glue -landroid -lEGL -lGLESv1_CM -llog -shared -o ..\lib\arm64-v8a\libgame.so
+echo %GXX% main.o -L. -landroid_native_app_glue -u ANativeActivity_onCreate -landroid -lEGL -lGLESv1_CM -llog -shared -o ..\%OUT_LIB_DIR%\libgame.so
+call %GXX% main.o -L. -landroid_native_app_glue -u ANativeActivity_onCreate -landroid -lEGL -lGLESv1_CM -llog -shared -o ..\%OUT_LIB_DIR%\libgame.so
 
 popd
 
@@ -121,26 +131,28 @@ call %BUILD_TOOLS%\aapt package -v -f -M AndroidManifest.xml -S res -I %ANDROID_
 REM ------------------------------------------------------
 REM Add the native activity and other libs to the APK
 
-call %BUILD_TOOLS%\aapt add bin\NativeActivity.unaligned.apk lib\arm64-v8a\libgame.so
+call %BUILD_TOOLS%\aapt add bin\NativeActivity.unaligned.apk %OUT_LIB_DIR%\libgame.so
+call %BUILD_TOOLS%\aapt add bin\NativeActivity.unaligned.apk %OUT_LIB_DIR%\liblog.so
+call %BUILD_TOOLS%\aapt add bin\NativeActivity.unaligned.apk %OUT_LIB_DIR%\libmyapplication.so
 
-copy "%TOOLCHAIN_LIB_DIR%\libc++_shared.so" lib\arm64-v8a\
-call %BUILD_TOOLS%\aapt add bin\NativeActivity.unaligned.apk "lib\arm64-v8a\libc++_shared.so"
+REM copy "%TOOLCHAIN_LIB_DIR%\libc++_shared.so" %OUT_LIB_DIR%\
+REM call %BUILD_TOOLS%\aapt add bin\NativeActivity.unaligned.apk "%OUT_LIB_DIR%\libc++_shared.so"
 
 REM mkdir lib\arm64 2>nul
-REM copy lib\arm64-v8a\*.* lib\arm64
+REM copy %OUT_LIB_DIR%\*.* lib\arm64
 REM call %BUILD_TOOLS%\aapt add bin\NativeActivity.unaligned.apk "lib\arm64\libgame.so"
 REM call %BUILD_TOOLS%\aapt add bin\NativeActivity.unaligned.apk "lib\arm64\libc++_shared.so"
 
-REM copy "%TOOLCHAIN_LIB_DIR%\33\libc++.so" lib\arm64-v8a\
-REM call %BUILD_TOOLS%\aapt add bin\NativeActivity.unaligned.apk "lib\arm64-v8a\libc++.so"
-REM copy "%TOOLCHAIN_LIB_DIR%\33\libandroid.so" lib\arm64-v8a\
-REM call %BUILD_TOOLS%\aapt add bin\NativeActivity.unaligned.apk "lib\arm64-v8a\libandroid.so"
-REM copy "%TOOLCHAIN_LIB_DIR%\33\liblog.so" lib\arm64-v8a\
-REM call %BUILD_TOOLS%\aapt add bin\NativeActivity.unaligned.apk "lib\arm64-v8a\liblog.so"
-REM copy "%TOOLCHAIN_LIB_DIR%\33\libEGL.so" lib\arm64-v8a\
-REM call %BUILD_TOOLS%\aapt add bin\NativeActivity.unaligned.apk "lib\arm64-v8a\libEGL.so"
-REM copy "%TOOLCHAIN_LIB_DIR%\33\libGLESv1_CM.so" lib\arm64-v8a\
-REM call %BUILD_TOOLS%\aapt add bin\NativeActivity.unaligned.apk "lib\arm64-v8a\libGLESv1_CM.so"
+REM copy "%TOOLCHAIN_LIB_DIR%\33\libc++.so" %OUT_LIB_DIR%\
+REM call %BUILD_TOOLS%\aapt add bin\NativeActivity.unaligned.apk "%OUT_LIB_DIR%\libc++.so"
+REM copy "%TOOLCHAIN_LIB_DIR%\33\libandroid.so" %OUT_LIB_DIR%\
+REM call %BUILD_TOOLS%\aapt add bin\NativeActivity.unaligned.apk "%OUT_LIB_DIR%\libandroid.so"
+REM copy "%TOOLCHAIN_LIB_DIR%\33\liblog.so" %OUT_LIB_DIR%\
+REM call %BUILD_TOOLS%\aapt add bin\NativeActivity.unaligned.apk "%OUT_LIB_DIR%\liblog.so"
+REM copy "%TOOLCHAIN_LIB_DIR%\33\libEGL.so" %OUT_LIB_DIR%\
+REM call %BUILD_TOOLS%\aapt add bin\NativeActivity.unaligned.apk "%OUT_LIB_DIR%\libEGL.so"
+REM copy "%TOOLCHAIN_LIB_DIR%\33\libGLESv1_CM.so" %OUT_LIB_DIR%\
+REM call %BUILD_TOOLS%\aapt add bin\NativeActivity.unaligned.apk "%OUT_LIB_DIR%\libGLESv1_CM.so"
 
 
 
