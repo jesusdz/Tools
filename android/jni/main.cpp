265a0a5cfc2b7cbad6097378170a98d949664a00
Jesus Diaz <jesusdz@gmail.com>
2023-12-15 03:58:33 +0100

    android/main.cpp: Minimal code in main(). It simply sets the callbacks and calls PlatformRun().

--- android/jni/main.cpp
+++ android/jni/main.cpp
@@ -1,25 +1,5 @@
 #include "main_vulkan.cpp"
 
-void PlatformWindowInit(Platform &platform)
-{
-	Graphics &gfx = GetPlatformGraphics(platform);
-
-	if ( !InitializeGraphics(platform.globalArena, platform.window, gfx) )
-	{
-		LOG(Error, "InitializeGraphics failed!");
-		return;
-	}
-
-	InitializeScene(gfx);
-}
-
-void PlatformWindowCleanup(Platform &platform)
-{
-	Graphics &gfx = GetPlatformGraphics(platform);
-
-	CleanupGraphics(gfx);
-}
-
 void android_main(struct android_app* app)
 {
 	Platform platform = {};
@@ -34,53 +14,11 @@ void android_main(struct android_app* app)
 	platform.CleanupCallback = EngineCleanup;
 	platform.WindowInitCallback = EngineWindowInit;
 	platform.WindowCleanupCallback = EngineWindowCleanup;
+	platform.androidApp = app;
 
 	// User data
 	Graphics gfx = {};
 	platform.userData = &gfx;
 
 	PlatformRun(platform);
-
-#if 0
-
-	Window &window = platform.window;
-	Arena &globalArena = platform.globalArena;
-	Arena &frameArena = platform.frameArena;
-
-	Graphics gfx = {};
-	platform.userData = &gfx;
-	platform.WindowInitCallback = PlatformWindowInit;
-	platform.WindowCleanupCallback = PlatformWindowCleanup;
-
-	Clock lastFrameClock = GetClock();
-
-	// Application loop
-	while ( 1 )
-	{
-		const Clock currentFrameClock = GetClock();
-		const f32 deltaSeconds = GetSecondsElapsed(lastFrameClock, currentFrameClock);
-		lastFrameClock = currentFrameClock;
-
-		PlatformUpdateEventLoop(platform);
-
-#if USE_CAMERA_MOVEMENT
-		AnimateCamera(window, gfx.camera, deltaSeconds);
-#endif
-
-		if ( window.flags & WindowFlags_Exiting )
-		{
-			break;
-		}
-		if ( window.keyboard.keys[KEY_ESCAPE] == KEY_STATE_PRESSED )
-		{
-			break;
-		}
-
-		if ( gfx.initialized )
-		{
-			RenderGraphics(gfx, window, frameArena, deltaSeconds);
-			ResetArena(frameArena);
-		}
-	}
-#endif
 }
