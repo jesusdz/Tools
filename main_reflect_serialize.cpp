711dff792382da7c799e9bf1462a618bab4d3d6c
Jesus Diaz <jesusdz@gmail.com>
2024-04-04 23:30:26 +0200

    main_reflect_serialize.cpp: Separated Print from PrintTrivial.

--- main_reflect_serialize.cpp
+++ main_reflect_serialize.cpp
@@ -140,102 +140,102 @@ const ReflexStruct* ReflexGetStruct(ReflexID id)
 	return reflections[id - ReflexID_Struct];
 }
 
-void Print(const void *data, const ReflexID id, bool isPointer)
+void PrintTrivial(const void *data, const ReflexID id, bool isPointer)
 {
-	if (ReflexIsTrivial(id))
-	{
-		const ReflexTrivial *trivial = ReflexGetTrivial(id);
-		//const u32 elemCount = trivial->elemCount;
+	const ReflexTrivial *trivial = ReflexGetTrivial(id);
+	//const u32 elemCount = trivial->elemCount;
 
-		//if (elemCount > 1) {
-		//	Printf("[");
-		//}
+	//if (elemCount > 1) {
+	//	Printf("[");
+	//}
 
-		//for (u32 i = 0; i < elemCount; ++i)
-		//{
-		//	if (i > 0) {
-		//		Printf(", ");
-		//	}
-			const u32 i = 0;
+	//for (u32 i = 0; i < elemCount; ++i)
+	//{
+	//	if (i > 0) {
+	//		Printf(", ");
+	//	}
+	const u32 i = 0;
 
-			const char *sVal = (const char *)data;
-			const bool bVal = *((bool*)data + i);
-			const char cVal = *((char*)data + i);
-			const i32 iVal = *((i32*)data + i);
-			const u32 uVal = *((u32*)data + i);
-			const float fVal = *((float*)data + i);
+	const char *sVal = (const char *)data;
+	const bool bVal = *((bool*)data + i);
+	const char cVal = *((char*)data + i);
+	const i32 iVal = *((i32*)data + i);
+	const u32 uVal = *((u32*)data + i);
+	const float fVal = *((float*)data + i);
 
-			if (trivial->isBool) {
-				Printf("%d", bVal ? 1 : 0);
-			} else if (trivial->isChar) {
-				if ( isPointer )
-				{
-					Printf("\"%s\"", sVal);
-				}
-				else
-				{
-					Printf("%c", cVal);
-				}
-			} else if (trivial->isFloat) {
-				Printf("%f", fVal);
-			} else if (trivial->isUnsigned) {
-				Printf("%u", uVal);
-			} else {
-				Printf("%d", iVal);
-			}
-		//}
-
-		//if (elemCount > 1) {
-		//	Printf("]");
-		//}
+	if (trivial->isBool) {
+		Printf("%d", bVal ? 1 : 0);
+	} else if (trivial->isChar) {
+		if (isPointer) {
+			Printf("\"%s\"", sVal);
+		} else {
+			Printf("%c", cVal);
+		}
+	} else if (trivial->isFloat) {
+		Printf("%f", fVal);
+	} else if (trivial->isUnsigned) {
+		Printf("%u", uVal);
+	} else {
+		Printf("%d", iVal);
 	}
-	else if (ReflexIsStruct(id))
+	//}
+
+	//if (elemCount > 1) {
+	//	Printf("]");
+	//}
+}
+
+void Print(const void *data, const ReflexID id)
+{
+	const ReflexStruct *rstruct = ReflexGetStruct(id);
+
+	PrintBeginScope("{");
+	PrintNewLine();
+
+	for (u32 i = 0; i < rstruct->memberCount; ++i)
 	{
-		const ReflexStruct *rstruct = ReflexGetStruct(id);
+		const ReflexMember *member = &rstruct->members[i];
+		const void *structPtr = data;
+		const void *memberPtr = ReflexMemberPtr(structPtr, member);
 
-		PrintBeginScope("{");
-		PrintNewLine();
+		Printf("\"%s\" : ", member->name);
 
-		for (u32 i = 0; i < rstruct->memberCount; ++i)
-		{
-			const ReflexMember *member = &rstruct->members[i];
-			const void *structPtr = data;
-			const void *memberPtr = ReflexMemberPtr(structPtr, member);
-
-			Printf("\"%s\" : ", member->name);
-
-			const u32 elemCount = ReflexGetElemCount(structPtr, rstruct, member->name);
-			if ( elemCount > 0 ) {
-				PrintBeginScope("[");
-				PrintNewLine();
-			}
-
-			const u32 elemSize = ReflexGetTypeSize(member->reflexId);
-			for (u32 elemIndex = 0; elemIndex == 0 || elemIndex < elemCount; ++elemIndex)
-			{
-				const void *elemPtr = (u8*)memberPtr + elemIndex * elemSize;
-				Print(elemPtr, member->reflexId, member->isPointer);
-
-				if (elemCount > 0 && elemIndex + 1 < elemCount) {
-					Printf(",");
-					PrintNewLine();
-				}
-			}
-
-			if ( elemCount > 0 ) {
-				PrintNewLine();
-				PrintEndScope("]");
-			}
-
-			if ( i + 1 < rstruct->memberCount ) {
-				Printf(",");
-			}
+		const u32 elemCount = ReflexGetElemCount(structPtr, rstruct, member->name);
+		if ( elemCount > 0 ) {
+			PrintBeginScope("[");
 			PrintNewLine();
-
 		}
 
-		PrintEndScope("}");
+		const u32 elemSize = ReflexGetTypeSize(member->reflexId);
+		for (u32 elemIndex = 0; elemIndex == 0 || elemIndex < elemCount; ++elemIndex)
+		{
+			const void *elemPtr = (u8*)memberPtr + elemIndex * elemSize;
+
+			if ( ReflexIsStruct(member->reflexId) ) {
+				Print(elemPtr, member->reflexId);
+			} else {
+				PrintTrivial(elemPtr, member->reflexId, member->isPointer);
+			}
+
+			if (elemCount > 0 && elemIndex + 1 < elemCount) {
+				Printf(",");
+				PrintNewLine();
+			}
+		}
+
+		if ( elemCount > 0 ) {
+			PrintNewLine();
+			PrintEndScope("]");
+		}
+
+		if ( i + 1 < rstruct->memberCount ) {
+			Printf(",");
+		}
+		PrintNewLine();
+
 	}
+
+	PrintEndScope("}");
 }
 
 
@@ -244,7 +244,7 @@ void Print(const void *data, const ReflexID id, bool isPointer)
 
 int main(int argc, char **argv)
 {
-	Print(&gAssets, REFLEX_ID(Assets), false);
+	Print(&gAssets, REFLEX_ID(Assets));
 
 	return 0;
 }
