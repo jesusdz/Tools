aff535237ba281ee897e0725e9b71376ebdb9a20
Jesus Diaz <jesusdz@gmail.com>
2025-07-05 14:54:33 +0200

    tools.h: Test code to open directories.

--- tools.h
+++ tools.h
@@ -61,6 +61,7 @@
 #include <string.h>   // strerror_r
 #include <errno.h>    // errno
 #include <sys/mman.h> // mmap
+#include <dirent.h>   // opendir/readdir/closedir
 #endif
 
 #if PLATFORM_LINUX || PLATFORM_ANDROID
@@ -3298,6 +3299,61 @@ bool PlatformInitialize(Platform &platform, int argc, char **argv)
 #endif // PLATFORM_ANDROID
 
 	InitializeDirectories(platform, argc, argv);
+
+#if 0 // Read directory test
+#if PLATFORM_LINUX
+	FilePath filepath = MakePath(ProjectDir, "assets");
+	DIR *d;
+	struct dirent *dir;
+	d = opendir(filepath.str);
+	if (d) {
+		while ((dir = readdir(d)) != NULL) {
+			LOG(Debug, "%s\n", dir->d_name);
+		}
+		closedir(d);
+	}
+#elif PLATFORM_WINDOWS
+	    WIN32_FIND_DATA fdFile;
+    HANDLE hFind = NULL;
+
+    char sPath[2048];
+
+    //Specify a file mask. *.* = We want everything!
+    sprintf(sPath, "%s\\*.*", sDir);
+
+    if((hFind = FindFirstFile(sPath, &fdFile)) == INVALID_HANDLE_VALUE)
+    {
+        printf("Path not found: [%s]\n", sDir);
+        return false;
+    }
+
+    do
+    {
+        //Find first file will always return "."
+        //    and ".." as the first two directories.
+        if(strcmp(fdFile.cFileName, ".") != 0
+                && strcmp(fdFile.cFileName, "..") != 0)
+        {
+            //Build up our file path using the passed in
+            //  [sDir] and the file/foldername we just found:
+            sprintf(sPath, "%s\\%s", sDir, fdFile.cFileName);
+
+            //Is the entity a File or Folder?
+            if(fdFile.dwFileAttributes &FILE_ATTRIBUTE_DIRECTORY)
+            {
+                printf("Directory: %s\n", sPath);
+                ListDirectoryContents(sPath); //Recursion, I love it!
+            }
+            else{
+                printf("File: %s\n", sPath);
+            }
+        }
+    }
+    while(FindNextFile(hFind, &fdFile)); //Find the next file.
+
+    FindClose(hFind); //Always, Always, clean things up!
+#endif
+#endif
 
 	return true;
 }
